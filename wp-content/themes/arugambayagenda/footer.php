61328cd34f66e90cea8bc2eb46213c05b561e350
Karu97 <karunanayake234@gmail.com>
2024-07-16 22:42:02 +0530

    Test

--- wp-content/themes/arugambayagenda/footer.php
+++ wp-content/themes/arugambayagenda/footer.php
@@ -142,59 +142,52 @@
                     <div class="row">
                         <div class="col-xl-6">
                             <div class="mil-field-frame mil-mb-20">
-                                <label>Check-in</label>
-                                <input id="check-in-2" type="text" class="datepicker-here" data-position="bottom left" placeholder="Select date" autocomplete="off" readonly="readonly">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                    <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                    <line x1="16" y1="2" x2="16" y2="6"></line>
-                                    <line x1="8" y1="2" x2="8" y2="6"></line>
-                                    <line x1="3" y1="10" x2="21" y2="10"></line>
-                                </svg>
+                                <label>Name</label>
+                                <input type="text" placeholder="Enter Full Name" value="1">
                             </div>
                         </div> 
                         <div class="col-xl-6">       
-                            <div class="mil-field-frame mil-mb-20">
-                                <label>Check-out</label>
-                                <input id="check-out-2" type="text" class="datepicker-here" data-position="bottom left" placeholder="Select date" autocomplete="off" readonly="readonly">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-                                    <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                                    <line x1="16" y1="2" x2="16" y2="6"></line>
-                                    <line x1="8" y1="2" x2="8" y2="6"></line>
-                                    <line x1="3" y1="10" x2="21" y2="10"></line>
-                                </svg>
+                        <div class="mil-field-frame mil-mb-20">
+                                <label>Email</label>
+                                <input type="text" placeholder="Enter Email Address" value="1">
                             </div>
                         </div>
                         <div class="col-xl-6">      
                             <div class="mil-field-frame mil-mb-20">
-                                <label>Adults</label>
-                                <input type="text" placeholder="Enter quantity" value="1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
-                                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                    <circle cx="9" cy="7" r="4"></circle>
-                                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                                    <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                                </svg>
+                                <label>WhatsApp Number</label>
+                                <input type="text" placeholder="Enter Number of Adults" value="1">
                             </div>
                         </div>  
                         <div class="col-xl-6">      
                             <div class="mil-field-frame mil-mb-20">
-                                <label>Adults</label>
+                                <label>Number of Adults</label>
                                 <input type="text" placeholder="Enter quantity" value="1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
-                                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                    <circle cx="9" cy="7" r="4"></circle>
-                                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                                    <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                                </svg>
                             </div>
-                        </div>      
-                            <button type="submit" class="mil-button mil-accent-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
-                                    <circle cx="11" cy="11" r="8"></circle>
-                                    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                                </svg>
-                                <span>Search</span>
-                            </button>
+                        </div>
+                        <div class="col-xl-6">      
+                            <div class="mil-field-frame mil-mb-20">
+                                <label>Website</label>
+                                <input type="text" placeholder="Enter Website" value="1">
+                            </div>
+                        </div>
+                        <div class="col-xl-12">
+                            <div class="mil-field-frame mil-mb-20">
+                                <label>Message</label>
+                                <textarea placeholder="Write a message here"></textarea>
+                            </div>
+                        </div>        
+                        <div class="row align-items-center">
+                            <div class="col-lg-7">
+                                <p class="mil-fade-up"><span class="mil-accent-2">*</span>We promise not to disclose your personal information to third parties.</p>
+                            </div>
+                            <div class="col-lg-5">
+                                <div class="mil-desctop-right mil-fade-up">
+                                    <button type="submit" class="mil-button">
+                                        <span>Send</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
