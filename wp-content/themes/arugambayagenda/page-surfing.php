8c95881d3c38d390a221329866646e04c4b5492c
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-17 21:19:33 +0530

    Test

--- wp-content/themes/arugambayagenda/page-surfing.php
+++ wp-content/themes/arugambayagenda/page-surfing.php
@@ -199,7 +199,7 @@ get_header();
                             <a href="<?php the_permalink(); ?>">
                                 <div class="mil-descr">
                                     <h3 class="mil-mb-20"><?php the_title(); ?></h3>
-                                    <p class="mil-mb-40">Accusantium doloremque laudantium, totam rem aperiam beatae vitae dicta sunt, explicabo</p>
+                                    <p class="mil-mb-40"><?php echo get_the_excerpt(); ?></p>
                                     <div class="mil-divider"></div>
                                     <div class="mil-card-bottom">
                                         <div class="mil-price"><span class="mil-symbol">$</span><span class="mil-number"><?php echo get_field('price_per_night'); ?></span>/ <?php echo get_field('duration'); ?> hour</div>
