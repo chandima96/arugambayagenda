83f627b03a34fb4f4412c78bcc1a219b1d53f8a9
Karu97 <karunanayake234@gmail.com>
2024-07-17 22:34:38 +0530

    Test

--- wp-content/themes/arugambayagenda/single-experientialtours.php
+++ wp-content/themes/arugambayagenda/single-experientialtours.php
@@ -246,13 +246,13 @@ $seventh_section_title = get_field('seventh_section_title');
 
                         <h3 class="mil-fade-up mil-mb-40">FAQ</h3>
                         <div class="mil-faq-section mil-mb-100">
-                        <div class="mil-faq-item mil-fade-up">
+                            <div class="mil-faq-item mil-fade-up">
                                 <div class="mil-faq-question">
                                     <span class="mil-icon">+</span>
-                                    <h3><?php echo $faq__section_question_one; ?> </h3>
+                                    <h3><?php echo $faq__section_question_two; ?> </h3>
                                 </div>
                                 <div class="mil-faq-answer">
-                                    <div class="custom-list"><?php echo $faq_section_answer_one; ?></div>
+                                    <div class="custom-list"><?php echo $faq_section_answer_two; ?></div>
                                 </div>
                             </div>
 
