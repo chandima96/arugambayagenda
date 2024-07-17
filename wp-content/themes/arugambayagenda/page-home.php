c1786c53df30f3e87b11c4b85c29d44a36d7b2f3
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-17 18:19:09 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -375,7 +375,7 @@ $our_blog_description = get_field('our_blog_description');
                                         <p class="mil-mb-40">Accusantium doloremque laudantium, totam rem aperiam beatae vitae dicta sunt, explicabo</p>
                                         <div class="mil-divider"></div>
                                             <div class="mil-card-bottom">
-                                                <div class="mil-price"><span class="mil-symbol">$</span><span class="mil-number">49</span>/per night</div>
+                                                <div class="mil-price"><span class="mil-symbol">$</span><span class="mil-number"><?php echo get_field('price_per_night'); ?></span>/ <?php echo get_field('duration'); ?> hour</div>
                                                 <a href="<?php the_permalink(); ?>" class="mil-button mil-icon-button mil-accent-1">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark">
                                                         <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
