10b4afdaa442226687824b7f8c9d7b32f6c638e0
Karu97 <karunanayake234@gmail.com>
2024-07-18 14:35:22 +0530

    Test

--- wp-content/themes/arugambayagenda/page-ride-with-us.php
+++ wp-content/themes/arugambayagenda/page-ride-with-us.php
@@ -130,11 +130,10 @@ get_header();
                                     <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                                 </svg>
                             </div>
-                            <h3 class="mil-mb-20">+27 (843) 38X 83 04</h3>
+                            <div class="mil-mb-20 contact-custom-text">+94766899188 <br> +94726479635</div>
                             <p>Call us anytime. We are available 24/7</p>
                         </div>
                     </div>
-
                     <div class="col-xl-4">
                         <div class="mil-iconbox mil-mb-40-adapt mil-fade-up">
                             <div class="mil-bg-icon"></div>
@@ -144,24 +143,10 @@ get_header();
                                     <polyline points="22,6 12,13 2,6"></polyline>
                                 </svg>
                             </div>
-                            <h3 class="mil-mb-20">aquarel.hotel@mail. com</h3>
+                            <div class="contact-custom-text mil-mb-20">info@arugambayagenda. com</div>
                             <p>Write to us. We respond quickly</p>
                         </div>
                     </div>
-
-                    <!-- <div class="col-xl-4">
-                        <div class="mil-iconbox mil-mb-40-adapt mil-fade-up">
-                            <div class="mil-bg-icon"></div>
-                            <div class="mil-icon mil-icon-fix">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
-                                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                                    <polyline points="22,6 12,13 2,6"></polyline>
-                                </svg>
-                            </div>
-                            <h3 class="mil-mb-20">aquarel.hotel@mail.com</h3>
-                            <p>Write to us. We respond quickly</p>
-                        </div>
-                    </div> -->
                     <div class="col-xl-4">
                         <div class="mil-iconbox mil-mb-40-adapt mil-fade-up">
                             <div class="mil-bg-icon"></div>
@@ -171,7 +156,8 @@ get_header();
                                     <circle cx="12" cy="10" r="3"></circle>
                                 </svg>
                             </div>
-                            <h3 class="mil-mb-20">Wharf st. 7584, some City</h3>
+                            <div class="mil-mb-20 contact-custom-text">No.54, Tsunami Hotel Road, Arugambay, 32500 Pottuvil, Srilanka.</div>
+                            
                             <p>We invite you to visit us</p>
                         </div>
                     </div>
