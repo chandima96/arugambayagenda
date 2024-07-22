0547c182e114b7d99cb1e235926606f9e4ac4ad1
Karu97 <karunanayake234@gmail.com>
2024-07-22 16:18:33 +0530

    Test

--- wp-content/themes/arugambayagenda/page-blog.php
+++ wp-content/themes/arugambayagenda/page-blog.php
@@ -288,7 +288,7 @@ get_header();
                     <div class="swiper-wrapper">
                                 <?php 
                                     $the_query = new WP_Query(array(
-                                        'post_type' => array('post'),
+                                        'post_type' => 'post',
                                         'posts_per_page' => 100,
                                         'post__not_in' => array($id),
                                     ));
@@ -299,25 +299,30 @@ get_header();
                                         $thumbnail_id = get_post_thumbnail_id();
                                         $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'full', true);
                                         $thumbnail_meta = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-                                ?> 
+                                ?>
                         <div class="swiper-slide">
-                            <div class="mil-card mil-mb-40-adapt mil-fade-up">
+                            <a href="<?php the_permalink(); ?>" class="mil-card mil-mb-40-adapt mil-fade-up">
                                 <div class="swiper-container mil-card-slider">
                                     <div class="swiper-wrapper">
                                         <div class="swiper-slide">
                                             <div class="mil-card-cover">
-                                                <img src="<?php echo get_field('image_one'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
-                                            </div>
+                                                <img src="<?php echo get_field('blog_image_one'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
+                                            </div>                                           
                                         </div>
                                         <div class="swiper-slide">
                                             <div class="mil-card-cover">
-                                                <img src="<?php echo get_field('image_two'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
-                                            </div>
+                                                <img src="<?php echo get_field('blog_image_two'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
+                                            </div>                                           
                                         </div>
                                         <div class="swiper-slide">
                                             <div class="mil-card-cover">
-                                                <img src="<?php echo get_field('image_three'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
-                                            </div>
+                                                <img src="<?php echo get_field('blog_image_three'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
+                                            </div>                                           
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="mil-card-cover">
+                                                <img src="<?php echo get_field('blog_image_four'); ?>" alt="cover" data-swiper-parallax="-100" data-swiper-parallax-scale="1.1">
+                                            </div>                                           
                                         </div>
                                     </div>
                                     <div class="mil-card-nav">
@@ -339,67 +344,42 @@ get_header();
                                 <ul class="mil-parameters">
                                     <li>
                                         <div class="mil-icon">
-                                            <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <g>
-                                                    <path d="M12.7432 5.75582C12.6516 7.02721 11.7084 8.00663 10.6799 8.00663C9.65144 8.00663 8.70673 7.02752 8.6167 5.75582C8.52291 4.43315 9.44106 3.505 10.6799 3.505C11.9188 3.505 12.837 4.45722 12.7432 5.75582Z" stroke="black" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M10.6793 10.0067C8.64232 10.0067 6.68345 11.0185 6.19272 12.9889C6.12771 13.2496 6.29118 13.5075 6.55905 13.5075H14.7999C15.0678 13.5075 15.2303 13.2496 15.1662 12.9889C14.6755 10.9869 12.7166 10.0067 10.6793 10.0067Z" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" />
-                                                    <path d="M6.42937 6.31713C6.3562 7.33276 5.59385 8.13264 4.77209 8.13264C3.95033 8.13264 3.18672 7.33308 3.1148 6.31713C3.04007 5.26053 3.7821 4.50537 4.77209 4.50537C5.76208 4.50537 6.50411 5.27992 6.42937 6.31713Z" stroke="black" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M6.61604 10.0688C6.05177 9.81023 5.4303 9.71082 4.77162 9.71082C3.14604 9.71082 1.57985 10.5189 1.18752 12.0929C1.13594 12.3011 1.26661 12.5071 1.48043 12.5071H4.99045" stroke="black" stroke-width="1.00189" stroke-miterlimit="10" stroke-linecap="round" />
-                                                </g>
-                                                <defs>
-                                                    <clipPath>
-                                                        <rect width="16.0035" height="16.0035" fill="white" transform="translate(0.176514 0.504028)" />
-                                                    </clipPath>
-                                                </defs>
-                                            </svg>
+                                            <img src="<?php echo get_template_directory_uri(); ?>/img/faces/6.jpg" alt="author">
                                         </div>
-                                        <div>Adults: <?php echo get_field('capacity'); ?></div>
+                                        <div><?php echo get_field('author'); ?></div>
                                     </li>
                                     <li>
                                         <div class="mil-icon">
                                             <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M10.9578 14.6084H12.7089C13.1733 14.6084 13.6187 14.4239 13.9471 14.0955C14.2755 13.7671 14.46 13.3217 14.46 12.8573V11.1062" stroke="black" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
-                                                <path d="M14.46 6.10644V4.35534C14.46 3.89092 14.2755 3.44553 13.9471 3.11713C13.6187 2.78874 13.1733 2.60425 12.7089 2.60425H10.9578" stroke="black" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
-                                                <path d="M5.95898 14.6084H4.20788C3.74346 14.6084 3.29806 14.4239 2.96967 14.0955C2.64128 13.7671 2.45679 13.3217 2.45679 12.8573V11.1062" stroke="black" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
-                                                <path d="M2.45679 6.10644V4.35534C2.45679 3.89092 2.64128 3.44553 2.96967 3.11713C3.29806 2.78874 3.74346 2.60425 4.20788 2.60425H5.95898" stroke="black" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M13.1881 2.62402H3.18597C2.35736 2.62402 1.68564 3.29574 1.68564 4.12435V13.1263C1.68564 13.9549 2.35736 14.6266 3.18597 14.6266H13.1881C14.0168 14.6266 14.6885 13.9549 14.6885 13.1263V4.12435C14.6885 3.29574 14.0168 2.62402 13.1881 2.62402Z" stroke="#272746" stroke-width="1.00189" stroke-linejoin="round" />
+                                                <path d="M4.18536 1.62305V2.63226" stroke="#272746" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M12.188 1.62305V2.63226" stroke="#272746" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M14.6885 5.12402H1.68564" stroke="#272746" stroke-width="1.00189" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
                                         </div>
-                                        <div>Kids: <?php echo get_field('size'); ?></div>
+                                        <div><?php echo get_field('published_date'); ?></div>
                                     </li>
                                 </ul>
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="mil-descr">
-                                        <h3 class="mil-mb-20"><?php the_title(); ?></h3>
-                                        <p class="mil-mb-40">Accusantium doloremque laudantium, totam rem aperiam beatae vitae dicta sunt, explicabo</p>
-                                        <div class="mil-divider"></div>
-                                            <div class="mil-card-bottom">
-                                                <div class="mil-price"><span class="mil-symbol">$</span><span class="mil-number"><?php echo get_field('price_per_night'); ?></span>/ <?php echo get_field('duration'); ?></div>
-                                                <a href="<?php the_permalink(); ?>" class="mil-button mil-icon-button mil-accent-1">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark">
-                                                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-                                                    </svg>
-                                                </a>
-                                            </div>
+                                <div class="mil-descr">
+                                    <h3 class="mil-mb-20"><?php the_title(); ?></h3>
+                                    <p class="mil-mb-20"><?php the_excerpt() ?></p>
+                                    <div class="mil-divider"></div>
+                                    <div class="mil-card-bottom">
+                                        <div class="mil-link">
+                                            <span>Read more</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right">
+                                                <line x1="5" y1="12" x2="19" y2="12"></line>
+                                                <polyline points="12 5 19 12 12 19"></polyline>
+                                            </svg>
                                         </div>
-                                    </a>
+                                    </div>
                                 </div>
-                            </div>
-                                <?php endwhile; endif; ?>
-                                <?php wp_reset_postdata(); ?>
+                            </a>
+
                         </div>
+                            <?php endwhile; endif; ?>
+                            <?php wp_reset_postdata(); ?>   
                     </div>
-                        <!-- <div class="row justify-content-between">
-                            <div class="col-lg-7">
-                                <p class="mil-fade-up">Accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
-                            </div>
-                            <div class="col-lg-5">
-                                <div class="mil-desctop-right mil-fade-up">
-                                    <a href="search.html" class="mil-button">
-                                        <span>View all</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div> -->
                 </div>
             </div>
         </div> 
