788f083b79e7bd5d721e332ab1818f3fc5309004
Karu97 <karunanayake234@gmail.com>
2024-07-22 14:03:21 +0530

    Test

--- wp-content/themes/arugambayagenda/page-activity.php
+++ wp-content/themes/arugambayagenda/page-activity.php
@@ -263,13 +263,13 @@ get_header();
                             </svg>
                             <span>Get in touch</span>
                         </a>
-                        <a href="<?php bloginfo('url'); ?>/all-tours" class="mil-link mil-open-book-popup">
+                        <!-- <a href="<?php bloginfo('url'); ?>/all-tours" class="mil-link mil-open-book-popup">
                             <span>Book now</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right">
                                 <line x1="5" y1="12" x2="19" y2="12"></line>
                                 <polyline points="12 5 19 12 12 19"></polyline>
                             </svg>
-                        </a>
+                        </a> -->
                     </span>
                 </div>
             </div>
--- wp-content/themes/arugambayagenda/single-surfing-tour.php
+++ wp-content/themes/arugambayagenda/single-surfing-tour.php
@@ -520,20 +520,20 @@ $seventh_section_title = get_field('seventh_section_title');
                     <div class="mil-suptitle mil-mb-20 mil-fade-up"><?php echo $seventh_section_top_line; ?></div>
                     <h2 class="mil-h2-lg mil-mb-40 mil-fade-up"><?php echo $seventh_section_title; ?></h2>
                     <span class="mil-buttons-frame mil-center mil-fade-up">
-                        <a href="contact.html" class="mil-button">
+                        <a href="<?php bloginfo('url'); ?>/contact-us" class="mil-button">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
                                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                 <polyline points="22,6 12,13 2,6"></polyline>
                             </svg>
                             <span>Get in touch</span>
                         </a>
-                        <a href="#." class="mil-link mil-open-book-popup">
+                        <!-- <a href="#." class="mil-link mil-open-book-popup">
                             <span>Book now</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right">
                                 <line x1="5" y1="12" x2="19" y2="12"></line>
                                 <polyline points="12 5 19 12 12 19"></polyline>
                             </svg>
-                        </a>
+                        </a> -->
                     </span>
                 </div>
             </div>
