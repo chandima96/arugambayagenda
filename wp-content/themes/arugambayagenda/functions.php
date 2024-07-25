021e0df75219f4c6afc9ed34e8ce176c0492ad05
Karu97 <karunanayake234@gmail.com>
2024-07-25 11:09:06 +0530

    Test

--- wp-content/themes/arugambayagenda/functions.php
+++ wp-content/themes/arugambayagenda/functions.php
@@ -9,11 +9,12 @@
  * @since Twenty Twenty-Two 1.0
  */
 
-function format_contact_number($number) {
+ function format_contact_number($number) {
     if (strlen($number) === 10) {
         return substr($number, 0, 3) . ' ' . substr($number, 3, 3) . ' ' . substr($number, 6, 2) . ' ' . substr($number, 8, 2);
     } else {
         return $number;
     }
 }
+
 
