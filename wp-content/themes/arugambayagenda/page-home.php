aecc2570a081590c2369c4cebb78b7315dcf82c2
Karu97 <karunanayake234@gmail.com>
2024-07-26 12:28:57 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -659,7 +659,20 @@ $review_description = get_field('review_description');
 
                     </div>
                 </div>
-
+                    <div class="mil-descr">
+                        <h3 class="mil-mb-20">The best travel insurance company for your holiday trip</h3>
+                        <p class="mil-mb-20">Accusantium doloremque laudantium, totam rem aperiam beatae vitae dicta sunt, explicabo</p>
+                        <div class="mil-divider"></div>
+                            <div class="mil-card-bottom">
+                                <div class="mil-link">
+                                        <span>More Reviews</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right">
+                                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                                            <polyline points="12 5 19 12 12 19"></polyline>
+                                        </svg>
+                                </div>
+                        </div>
+                    </div>
             </div>
         </div>
         <!-- reviews end -->
