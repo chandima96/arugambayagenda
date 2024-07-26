a9ddcfa5070eb060081cd0eb2d1d13ada31038a5
Karu97 <karunanayake234@gmail.com>
2024-07-26 12:46:28 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -172,6 +172,26 @@ $review_description = get_field('review_description');
         </div>
         <!-- services end -->
 
+        <!-- call to action -->
+        <div class="mil-content-pad mil-p-100-100 mil-fade-up">
+            <div class="container">
+                <div class="mil-text-center">
+                    <div class="mil-suptitle mil-mb-20 mil-fade-up">Taxi service</div>
+                    <h2 class="mil-h2-lg mil-mb-40 mil-fade-up">Ride with us</h2>
+                    <span class="mil-buttons-frame mil-center mil-fade-up">
+                        <a href="<?php bloginfo('url'); ?>/taxi-service" class="mil-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                                <polyline points="22,6 12,13 2,6"></polyline>
+                            </svg>
+                            <span>Book Your Taxi</span>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <!-- call to action end -->
+
         <!-- features -->
         <div class="mil-features mil-p-100-60">
             <img src="<?php echo get_template_directory_uri(); ?>/img/shapes/4.png" class="mil-shape mil-fade-up" style="width: 85%; top: -20%; left: -30%; transform: rotate(35deg)" alt="shape">
