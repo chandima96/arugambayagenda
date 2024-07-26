aea233b6a956fc7008b4c718d0d7db769845c739
Karu97 <karunanayake234@gmail.com>
2024-07-26 13:07:12 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -587,17 +587,18 @@ $review_description = get_field('review_description');
 
                                 <p>
                                     <?php
-                                    $contact_number = get_field('contact_number'); 
+                                        $contact_number = get_field('contact_number'); 
+                                        
+                                        if ($contact_number) {
+                                            $formatted_number = format_contact_number($contact_number);
 
-                                    if ($contact_number) {
-                                        $formatted_number = format_contact_number($contact_number);
-
-                                        echo '<a href="tel:' . $contact_number . '" class="contact-number">' . $formatted_number . '</a>';
-                                    } else {
-                                        echo 'Contact number not available';
-                                    }
+                                            echo '<a href="tel:' . $contact_number . '" class="contact-number">' . $formatted_number . '</a>';
+                                        } else {
+                                            echo 'Contact number not available';
+                                        }
                                     ?>
                                 </p>
+                                <p></p>
 
                             <div class="social-links">
                                 <a href="<?php echo get_field('favebook_link'); ?> " target="_blank" ><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/></svg></a>
