d4e39c063cff3ff60a441a684279c5b1a13e359e
Karu97 <karunanayake234@gmail.com>
2024-07-29 17:53:55 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -679,7 +679,9 @@ $counter_five = get_field('counter_five');
                                             <h3 class="mil-mb-10"><?php the_title(); ?></h3>
                                             <p class="mil-link"><?php the_content(); ?></p>
                                         </div>
-                                        <p class="mil-fade-up mil-mt-40"><span class="mil-fade-up"><?php the_excerpt(); ?></span></p>
+                                        <!-- <p class="mil-fade-up mil-mt-40"><?php the_excerpt(); ?></p> -->
+                                        <p class="mil-fade-up mil-mt-40">Sample excerpt content content content content content</p>
+
                                     </div>
                                 </div>
                                     <?php endwhile; endif; ?>
