91dfaa7061dbd999cbc3ecb12af022130f1a4acf
Karu97 <karunanayake234@gmail.com>
2024-07-29 16:42:54 +0530

    Test

--- wp-content/themes/arugambayagenda/header.php
+++ wp-content/themes/arugambayagenda/header.php
@@ -25,6 +25,10 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
     <!-- custom css -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/custom-styles.css">
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <!-- page name -->
     <?php wp_head(); ?>
 </head>
