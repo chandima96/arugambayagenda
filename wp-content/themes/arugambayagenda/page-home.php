dc0d2e80c612671ccdd3f5f6f824f87245592994
Karu97 <karunanayake234@gmail.com>
2024-07-29 16:21:08 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -642,25 +642,14 @@ $counter_five = get_field('counter_five');
                                     <a href="<?php echo get_field('linkedin_link'); ?>" target="_blank" ><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/></svg></a>        
                                 </div>
                             </div>
-                                <?php endwhile; endif; ?>
-                                <?php wp_reset_postdata(); ?>
                         </div>
+                            <?php endwhile; endif; ?>
+                            <?php wp_reset_postdata(); ?>
                     </div>
-                        <!-- <div class="row justify-content-between">
-                            <div class="col-lg-7">
-                                <p class="mil-fade-up">Accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.</p>
-                            </div>
-                            <div class="col-lg-5">
-                                <div class="mil-desctop-right mil-fade-up">
-                                    <a href="search.html" class="mil-button">
-                                        <span>View all</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div> -->
                 </div>
             </div>
         </div>    
+
         <!-- Team -->
         <!-- <div class="mil-content-pad mil-p-100-100">
             <div class="container">
