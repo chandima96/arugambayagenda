7f2c3dec8b263ee142e167a9948bc7e8b986e927
Karu97 <karunanayake234@gmail.com>
2024-07-29 14:43:04 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -563,7 +563,7 @@ $counter_five = get_field('counter_five');
         </div> -->
         <!-- about 2 end -->
 
-    <!-- Team -->
+        <!-- Team -->
         <div class="mil-content-pad mil-p-100-100">
             <div class="container">
                 <div class="mil-text-center">
@@ -573,66 +573,57 @@ $counter_five = get_field('counter_five');
                 <div class="swiper-container team-slider">
                     <div class="swiper-wrapper">
                         <?php 
-                        $the_query = new WP_Query(array(
-                            'post_type' => 'team',
-                            'posts_per_page' => 100,
-                            'post__not_in' => array($id),
-                        ));
+                            $the_query = new WP_Query(array(
+                                'post_type' => 'team',
+                                'posts_per_page' => 100,
+                                'post__not_in' => array($id),
+                            ));
                         ?>
                         <?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                        <?php
-                        $thumbnail_id = get_post_thumbnail_id();
-                        $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'full', true);
-                        $thumbnail_meta = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-                        ?>
-                        <div class="swiper-slide">
-                            <div class="team-member">
-                                <?php if (has_post_thumbnail()) : ?>
-                                <img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php echo $thumbnail_meta; ?>">
-                                <?php endif; ?>
-                                <h3><?php the_title(); ?></h3>
-                                <p class="margin-bottom-5"><?php echo get_field('designation'); ?></p>
-                                <p>
-                                    <?php
-                                    $contact_number = get_field('contact_number'); 
-                                    if ($contact_number) {
-                                        $formatted_number = format_contact_number($contact_number);
-                                        echo '<a href="tel:' . $contact_number . '" class="contact-number">' . $formatted_number . '</a>';
-                                    } else {
-                                        echo 'Contact number not available';
-                                    }
-                                    ?>
-                                </p>
-                                <div class="social-links">
-                                    <a href="<?php echo get_field('facebook_link'); ?>" target="_blank">
-                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                                        </svg>
-                                    </a>
-                                    <a href="<?php echo get_field('twitter_link'); ?>" target="_blank">
-                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
-                                        </svg>
-                                    </a>
-                                    <a href="<?php echo get_field('instagram_link'); ?>" target="_blank">
-                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.442a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334zm0 0"/>
-                                        </svg>
-                                    </a>
-                                    <a href="<?php echo get_field('linkedin_link'); ?>" target="_blank">
-                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M20.447 0H3.554C1.591 0 0 1.591 0 3.553v16.893C0 22.41 1.59 24 3.554 24h16.893C22.409 24 24 22.41 24 20.447V3.554C24 1.591 22.409 0 20.447 0zM7.125 20.249H3.896V9.04h3.229v11.21zM5.511 7.679c-1.034 0-1.87-.837-1.87-1.871s.837-1.87 1.87-1.87c1.034 0 1.871.837 1.871 1.87s-.837 1.871-1.871 1.871zm14.738 12.57h-3.229v-5.449c0-1.298-.025-2.97-1.812-2.97-1.814 0-2.093 1.417-2.093 2.88v5.539H9.885V9.04h3.099v1.526h.043c.432-.818 1.489-1.68 3.062-1.68 3.275 0 3.88 2.157 3.88 4.964v6.399z"/>
-                                        </svg>
-                                    </a>
+                            <?php
+                                $thumbnail_id = get_post_thumbnail_id();
+                                $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'full', true);
+                                $thumbnail_meta = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+                            ?>
+                            <div class="swiper-slide">
+                                <div class="team-member">
+                                    <?php if (has_post_thumbnail()) { ?>
+                                        <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>" alt="<?php echo esc_attr($thumbnail_meta); ?>">
+                                    <?php } ?>
+                                    <h3><?php the_title(); ?></h3>
+                                    <p class="margin-bottom-5"><?php echo get_field('designation'); ?></p>
+                                    <p>
+                                        <?php
+                                            $contact_number = get_field('contact_number'); 
+                                            if ($contact_number) {
+                                                $formatted_number = format_contact_number($contact_number);
+                                                echo '<a href="tel:' . $contact_number . '" class="contact-number">' . $formatted_number . '</a>';
+                                            } else {
+                                                echo 'Contact number not available';
+                                            }
+                                        ?>
+                                    </p>
+                                    <div class="social-links">
+                                        <a href="<?php echo get_field('facebook_link'); ?>" target="_blank" >[Facebook Icon]</a>
+                                        <a href="<?php echo get_field('twitter_link'); ?>" target="_blank" >[Twitter Icon]</a>
+                                        <a href="<?php echo get_field('instagram_link'); ?>" target="_blank" >[Instagram Icon]</a>
+                                        <a href="<?php echo get_field('linkedin_link'); ?>" target="_blank" >[LinkedIn Icon]</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <?php endwhile; wp_reset_postdata(); endif; ?>
+                        <?php endwhile; endif; ?>
+                        <?php wp_reset_postdata(); ?>
                     </div>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+                    <!-- Add Navigation -->
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
                 </div>
             </div>
         </div>
-    <!-- Team -->
+        <!-- Team End -->
+
 
         <!-- reviews -->
         <div class="mil-content-pad mil-p-100-100" style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/shapes/5.png); background-size: 100%; overflow-x: hidden;">
@@ -1077,6 +1068,40 @@ $counter_five = get_field('counter_five');
             });
         });
     </script>
+
+            <!-- Swiper JS and CSS inclusion -->
+            <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+        <!-- Initialize Swiper -->
+        <script>
+            var swiper = new Swiper('.team-slider', {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                breakpoints: {
+                    640: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    },
+                }
+            });
+        </script>
 
 
 
