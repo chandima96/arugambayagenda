8c73ee7834f89f0726ee2d1a9a42290aea41c12f
Karu97 <karunanayake234@gmail.com>
2024-07-29 18:00:54 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -687,6 +687,7 @@ $counter_five = get_field('counter_five');
                             </div>
                         </div>
 
+                        
                         <div class="mil-slider-nav mil-reviews-nav mil-fade-up">
                             <div class="mil-slider-arrow mil-prev mil-revi-prev mil-arrow-place">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right">
