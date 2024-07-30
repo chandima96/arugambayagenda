ea42a94be4d49dcf9b55c91b3c1d8328f1fbaa65
Karu97 <karunanayake234@gmail.com>
2024-07-30 11:11:29 +0530

    Test

--- wp-content/themes/arugambayagenda/single-wildlifetours.php
+++ wp-content/themes/arugambayagenda/single-wildlifetours.php
@@ -162,7 +162,7 @@ $seventh_section_title = get_field('seventh_section_title');
                         <!-- features -->
                         <div class="row mil-mb-60-adapt">
                             <div class="col-12">
-                                <h3 class="mil-fade-up mil-mb-40">Key features</h3>
+                                <h3 class="mil-fade-up mil-mb-40">About this activity</h3>
                             </div>
 
                             <?php if (!empty($feature_one_title)): ?>
@@ -513,12 +513,12 @@ $seventh_section_title = get_field('seventh_section_title');
                     <div class="mil-suptitle mil-mb-20 mil-fade-up"><?php echo $seventh_section_top_line; ?></div>
                     <h2 class="mil-h2-lg mil-mb-40 mil-fade-up"><?php echo $seventh_section_title; ?></h2>
                     <span class="mil-buttons-frame mil-center mil-fade-up">
-                        <a href="<?php bloginfo('url'); ?>/contact-us" class="mil-button">
+                        <a href="https://wa.me/94726479635" target="_blank" class="mil-button">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
                                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                 <polyline points="22,6 12,13 2,6"></polyline>
                             </svg>
-                            <span>Get in touch</span>
+                            <span>Chat with Hanas</span>
                         </a>
                         <!-- <a href="#." class="mil-link mil-open-book-popup">
                             <span>Book now</span>
