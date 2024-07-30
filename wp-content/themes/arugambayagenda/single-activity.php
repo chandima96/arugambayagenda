2fee49193c7222d7682f9d66fbabb86ce49b5e33
Karu97 <karunanayake234@gmail.com>
2024-07-30 11:02:03 +0530

    Test

--- wp-content/themes/arugambayagenda/single-activity.php
+++ wp-content/themes/arugambayagenda/single-activity.php
@@ -182,75 +182,75 @@ $seventh_section_title = get_field('seventh_section_title');
 
                             <?php if (!empty($feature_two_title)): ?>
                             <div class="col-xl-4">
-                                <div class="mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
+                                <div class="icon-box-padding-4 mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
                                     <div class="mil-bg-icon"></div>
-                                    <div class="mil-icon mil-icon-fix">
+                                    <div class="mil-icon mil-icon-fix icon-w-h">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
                                             <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
                                             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
                                         </svg>
                                     </div>
-                                    <h5><?php echo $feature_two_title; ?></h5>
+                                    <h5 class="feature-h5"><?php echo $feature_two_title; ?></h5>
                                 </div>
                             </div>
                             <?php endif; ?>
 
                             <?php if (!empty($feature_three_title)): ?>
                             <div class="col-xl-4">
-                                <div class="mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
+                                <div class="icon-box-padding-4 mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
                                     <div class="mil-bg-icon"></div>
-                                    <div class="mil-icon mil-icon-fix">
+                                    <div class="mil-icon mil-icon-fix icon-w-h">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
                                             <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
                                             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
                                         </svg>
                                     </div>
-                                    <h5><?php echo $feature_three_title; ?></h5>
+                                    <h5 class="feature-h5"><?php echo $feature_three_title; ?></h5>
                                 </div>
                             </div>
                             <?php endif; ?>
 
                             <?php if (!empty($feature_four_title)): ?>
                             <div class="col-xl-4">
-                                <div class="mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
+                                <div class="icon-box-padding-4 mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
                                     <div class="mil-bg-icon"></div>
-                                    <div class="mil-icon mil-icon-fix">
+                                    <div class="mil-icon mil-icon-fix icon-w-h">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
                                             <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
                                             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
                                         </svg>
                                     </div>
-                                    <h5><?php echo $feature_four_title; ?></h5>
+                                    <h5 class="feature-h5"><?php echo $feature_four_title; ?></h5>
                                 </div>
                             </div>
                             <?php endif; ?>
 
                             <?php if (!empty($feature_five_title)): ?>
                             <div class="col-xl-4">
-                                <div class="mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
+                                <div class="icon-box-padding-4 mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
                                     <div class="mil-bg-icon"></div>
-                                    <div class="mil-icon mil-icon-fix">
+                                    <div class="mil-icon mil-icon-fix icon-w-h">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
                                             <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
                                             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
                                         </svg>
                                     </div>
-                                    <h5><?php echo $feature_five_title; ?></h5>
+                                    <h5 class="feature-h5"><?php echo $feature_five_title; ?></h5>
                                 </div>
                             </div>
                             <?php endif; ?>
 
                             <?php if (!empty($feature_six_title)): ?>
                             <div class="col-xl-4">
-                                <div class="mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
+                                <div class="icon-box-padding-4 mil-iconbox mil-iconbox-sm mil-mb-40-adapt mil-fade-up">
                                     <div class="mil-bg-icon"></div>
-                                    <div class="mil-icon mil-icon-fix">
+                                    <div class="mil-icon mil-icon-fix icon-w-h">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
                                             <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
                                             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
                                         </svg>
                                     </div>
-                                    <h5><?php echo $feature_six_title; ?></h5>
+                                    <h5 class="feature-h5"><?php echo $feature_six_title; ?></h5>
                                 </div>
                             </div>
                             <?php endif; ?>
