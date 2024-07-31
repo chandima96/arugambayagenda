539ebd1e9cc889cba0b56ac83cc3dca3af4603ad
Karu97 <karunanayake234@gmail.com>
2024-07-31 21:32:10 +0530

    Test

--- wp-content/themes/arugambayagenda/functions.php
+++ wp-content/themes/arugambayagenda/functions.php
@@ -29,26 +29,3 @@ function display_page_slug() {
 add_shortcode('page_slug', 'display_page_slug');
 
 
-add_filter('wpcf7_form_hidden_fields', 'populate_hidden_fields');
-
-function populate_hidden_fields($hidden_fields) {
-    // Check if data is available in GET (URL parameters)
-    if (isset($_GET['date'])) {
-        $hidden_fields['date'] = htmlspecialchars($_GET['date']);
-    }
-    if (isset($_GET['time'])) {
-        $hidden_fields['time'] = htmlspecialchars($_GET['time']);
-    }
-    if (isset($_GET['pax'])) {
-        $hidden_fields['pax'] = htmlspecialchars($_GET['pax']);
-    }
-    if (isset($_GET['kids'])) {
-        $hidden_fields['kids'] = htmlspecialchars($_GET['kids']);
-    }
-    if (isset($_GET['total_amount'])) {
-        $hidden_fields['total_amount'] = htmlspecialchars($_GET['total_amount']);
-    }
-
-    return $hidden_fields;
-}
-
