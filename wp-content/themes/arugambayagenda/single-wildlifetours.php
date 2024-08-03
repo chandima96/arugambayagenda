6ea1fb569298885195b2c13aa259734a0a94f401
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-08-03 16:00:40 +0530

    Test

--- wp-content/themes/arugambayagenda/single-wildlifetours.php
+++ wp-content/themes/arugambayagenda/single-wildlifetours.php
@@ -722,7 +722,7 @@ $price_case_kids_ten = get_field('price_case_kids_ten');
                                         <p class="mil-mb-40"><?php echo get_field('description'); ?></p>
                                         <div class="mil-divider"></div>
                                         <div class="mil-card-bottom">
-                                            <div class="mil-price"><span class="mil-symbol">$</span><span class="mil-number"><?php echo get_field('price_per_night'); ?></span>/ <span>per pax /</span> <?php echo get_field('duration'); ?></div>
+                                            <div class="mil-price"><span class="mil-symbol">$</span><span class="mil-number"><?php echo get_field('price_case_two'); ?></span>/ <span>per pax /</span> <?php echo get_field('duration'); ?></div>
                                             <a href="<?php the_permalink(); ?>" class="mil-button mil-icon-button mil-accent-1">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bookmark">
                                                     <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
