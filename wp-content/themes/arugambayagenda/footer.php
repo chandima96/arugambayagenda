9aae0819aa0348fdb5a9452301d0a322d3e07c2a
Karu97 <karunanayake234@gmail.com>
2024-08-27 15:08:45 +0530

    Test

--- wp-content/themes/arugambayagenda/footer.php
+++ wp-content/themes/arugambayagenda/footer.php
@@ -235,7 +235,7 @@
 
     
         <!-- slick carousel scripts -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 
     <script>
