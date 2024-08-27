e38465660c58ffe77dfc428cfa2efbb96c1ac67a
Karu97 <karunanayake234@gmail.com>
2024-08-27 15:10:45 +0530

    Test

--- wp-content/themes/arugambayagenda/footer.php
+++ wp-content/themes/arugambayagenda/footer.php
@@ -235,41 +235,7 @@
 
     
         <!-- slick carousel scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 
-    <script>
-        $('.team-members').slick({
-            slidesToShow: 3,
-            speed: 300,
-            prevArrow: '#left-arrow',
-            nextArrow: '#right-arrow',
-            centerPadding: '60px',
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        infinite: true,
-                    }
-                },
-                {
-                    breakpoint: 900,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                    }
-                },
-                {
-                    breakpoint: 760,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                }]
-        });
-    </script>
     
     <?php wp_footer(); ?>
 </body>
