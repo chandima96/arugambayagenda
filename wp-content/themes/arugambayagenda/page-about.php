04b05be03fc6351e562415492d3d71eefa0117a2
Karu97 <karunanayake234@gmail.com>
2024-08-28 11:33:53 +0530

    Test

--- wp-content/themes/arugambayagenda/page-about.php
+++ wp-content/themes/arugambayagenda/page-about.php
@@ -211,8 +211,8 @@ get_header();
     }
 
     .team-member {
-        flex: 0 0 275px;
-        margin: 20px;
+        flex: 0 0 265px;
+        margin: 10px;
         background: white;
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
