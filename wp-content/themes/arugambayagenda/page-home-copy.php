94b65b10c0cf2782cd7581d44a56c0dea78cd385
Nikalanda Bandara <chandima96dilan@gmail.com>
2025-02-23 15:47:19 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home-copy.php
+++ wp-content/themes/arugambayagenda/page-home-copy.php
@@ -66,6 +66,12 @@ $counter_five = get_field('counter_five');
             .swiper-container {
             width: 100%;
             height: 100vh;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
         }
 
         .swiper-slide {
