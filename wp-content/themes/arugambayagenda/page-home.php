827a7ae6d3f7488b9d4fdbe884dacb2bf0ae2ee3
Nikalanda Bandara <chandima96dilan@gmail.com>
2025-03-03 18:06:09 +0530

    Test

--- wp-content/themes/arugambayagenda/page-home.php
+++ wp-content/themes/arugambayagenda/page-home.php
@@ -49,7 +49,7 @@ $counter_five = get_field('counter_five');
 ?>
         <div class="mil-banner">
             <div class="mil-banner-bg">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/images/IMG_1036.JPG" alt="background">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/images/IMG_1036.JPG" style="transform: scaleX(-1);" alt="background">
                 <div class="mil-image-gradient"></div>
             </div>
             <div class="container">
@@ -59,7 +59,9 @@ $counter_five = get_field('counter_five');
                         <div class="mil-banner-content-frame">
                             <div class="mil-banner-content">
                                 <div class="mil-suptitle mil-mb-40"><?php echo $home_banner_title; ?></div>
-                                <h1 class="mil-mb-40" style="text-shadow: -1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white;"><?php echo $home_banner_top_line; ?></h1>
+                                <h1 class="mil-mb-40" style="text-shadow: -1px -1px 0 #000000, 1px -1px 0 #000000, -1px 1px 0 #000000, 1px 1px 0 #000000;
+    color: #4fe4ff !important;
+    letter-spacing: 1px;"><?php echo $home_banner_top_line; ?></h1>
                                 <p style="text-shadow: -1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white;"><?php echo $home_banner_description; ?></p>
                             </div>
                         </div>
