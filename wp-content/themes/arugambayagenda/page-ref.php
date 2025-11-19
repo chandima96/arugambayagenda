5caae038d1055bf5c369da5990f87cdf7afe3c94
Nikalanda Bandara <chandima96dilan@gmail.com>
2025-11-19 22:47:59 +0530

    Test

--- wp-content/themes/arugambayagenda/page-ref.php
+++ wp-content/themes/arugambayagenda/page-ref.php
@@ -6,17 +6,38 @@ get_header();
 
 ?>
 <div class="mil-content-pad mil-p-100-100">
-<?php
-if ( isset($_GET['ref']) ) {
-    $ref = sanitize_text_field($_GET['ref']);
-    
-}
-?>
+
 </div>
 <div class="mil-content-pad mil-p-100-100">
 <?php    
-echo $ref;
+
+// Get ref from URL
+$ref = '';
+if ( isset($_GET['ref']) ) {
+    $ref = sanitize_text_field( wp_unslash($_GET['ref']) );
+}
+
+global $wpdb;
+$table = 'wp_ref';
+
+// If ref is provided
+if ( ! empty($ref) ) {
+
+    // Query DB for the matching ref
+    $result = $wpdb->get_row(
+        $wpdb->prepare("SELECT email FROM {$table} WHERE ref = %s", $ref)
+    );
+
+    // Display email if found
+    if ( $result ) {
+        echo '<p>Email: ' . esc_html($result->email) . '</p>';
+    } else {
+        echo '<p>No email found for this reference.</p>';
+    }
+}
 ?>
+
+
 </div>
 
 <?php get_footer(); ?>
