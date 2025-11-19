ba149b2fe6c94c1d0ac7cdabd89c188b8baa0292
Nikalanda Bandara <chandima96dilan@gmail.com>
2025-11-19 22:41:39 +0530

    Test

--- wp-content/themes/arugambayagenda/page-ref.php
+++ wp-content/themes/arugambayagenda/page-ref.php
@@ -5,6 +5,17 @@
 get_header();
 
 ?>
-
+<div class="mil-content-pad mil-p-100-100">
+<?php
+if ( isset($_GET['ref']) ) {
+    $ref = sanitize_text_field($_GET['ref']);
+    
+}
+?>
+<div class="mil-content-pad mil-p-100-100">
+<?php    
+echo $ref;
+?>
+</div>
 
 <?php get_footer(); ?>
